32f49332764e4ba49a87be501318d1042d03d9fb
Su-arPha <125963134+Su-arPha@users.noreply.github.com>
2023-12-07 14:11:46 +0100

    display demo project

---
+++ home.js
@@ -0,0 +1,35 @@
+let objProducts = [{
+     id: 1,
+     name: "Monstera Deliciosa",
+     desc: "The Swiss Cheese Plant",
+     img: "https://images.unsplash.com/photo-1637967885705-a60e3fea266d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2564&q=80",
+     price: 699,
+
+},{
+    id: 2,
+	name: "Musa acuminata",
+	desc: "The Dwarf Cavendish Banana",
+	img: "https://images.unsplash.com/photo-1638824097313-8a42fef7c87c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+	price: 799,
+},{
+    id: 3,
+	name: "Zamioculcas Zamiifolia",
+	desc: "Zanzibar Gem",
+	img: "https://images.unsplash.com/photo-1632207691143-643e2a9a9361?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+	price: 299,
+}, {
+    id: 4,
+	name: "Dracaena trifasciata",
+	desc: "Snake Plant",
+	img: "https://images.unsplash.com/photo-1638824096986-5c5ed96d118a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+	price: 259,
+},
+{
+    id: 5,
+    name: "Pilea Peperomioides",
+    desc: "The Chinese Money Plant ",
+    img: "https://images.unsplash.com/photo-1614594805320-e6a5549d7f95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+    price: 199,
+},
+] 
+    
---
+++ products.js
@@ -0,0 +1,155 @@
+let cartIcon = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close');
+let body = document.querySelector('body');
+let listCarttoHTML = document.querySelector('.listCart ');
+let listProductItem = document.querySelector('.listProducts');
+let iconCartSpan = document.querySelector('.icon-cart span');
+let listProducts =[];
+let cart = [];
+
+cartIcon.addEventListener('click', () => {
+  body.classList.toggle('showCart')
+})
+closeCart.addEventListener('click', () => {
+  body.classList.toggle('showCart')
+})
+
+
+
+// Add to Cart Functionality
+
+const addDataProductItem = () => {
+    listProductItem.innerHTML = '';
+
+    if(listProducts.length > 0){
+
+        listProducts.forEach(product => {
+        let newProduct = document.createElement('div');
+        newProduct.classList.add('item');
+        newProduct.dataset.id = product.id;
+        newProduct.innerHTML = `
+
+        <img src="${product.img}" alt="">
+        <h2>${product.name}</h2>
+        <p class="price">$ ${product.price}</p>
+        <button class="addCart">
+            Add To Cart
+        </button>
+        `;
+        listProductItem.appendChild(newProduct);
+      } )
+    }
+}
+listProductItem.addEventListener('click', (event) => {
+  let buttonClick = event.target;
+  if(buttonClick.classList.contains('addCart')){
+    let product_id = buttonClick.parentElement.dataset.id;
+    addtoCart(product_id);
+  }
+})
+
+const addtoCart = (product_id) => {
+  let itemProductInCart = cart.findIndex((value) => value.product_id == product_id);
+  if(cart.length <= 0) {
+    cart = [{
+      product_id: product_id,
+      quantity: 1
+    }]
+  }else if(itemProductInCart < 0 ){
+    cart.push({
+      product_id: product_id,
+      quantity: 1
+    });
+  }else{
+    cart[itemProductInCart].quantity = cart[itemProductInCart].quantity + 1; 
+  }
+  addItemCartToHTML();
+  addItemCartToLocalStorage();
+}
+const addItemCartToLocalStorage = () => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+const addItemCartToHTML = () => {
+  listCarttoHTML.innerHTML = '';
+  let totalQuantity = 0;
+  if(cart.length > 0){
+    cart.forEach(itemCart => {
+      totalQuantity = totalQuantity + itemCart.quantity;
+      let newCartItem = document.createElement('div');
+      newCartItem.classList.add('itemCart');
+      newCartItem.dataset.id = itemCart.product_id;
+
+      let itemProduct = listProducts.findIndex((value) => value.id == itemCart.product_id);
+      let itemInfo = listProducts[itemProduct];
+      listCarttoHTML.appendChild(newCartItem);
+      newCartItem.innerHTML = `
+      <div class="image">
+        <img src="${itemInfo.img}" alt="" />
+      </div>
+      <div class="name">
+            <h2>${itemInfo.name}</h2>
+      </div>
+      <div class="totalPrice">
+            <p>${itemInfo.price * itemCart.quantity}</p>
+      </div>
+      <div class="quantity">
+      <span class="plus">+</span>
+      <span>${itemCart.quantity}</span>
+      <span class="minus">-</span>
+    </div>
+    `;
+   
+    })
+}
+iconCartSpan.innerText = totalQuantity;
+}
+listCarttoHTML.addEventListener('click', (event) => {
+  let itemClick = event.target;
+  if(itemClick.classList.contains('minus') || itemClick.classList.contains('plus')){
+    let product_id  = itemClick.parentElement.parentElement.dataset.id;
+    let type = 'minus';
+    if(itemClick.classList.contains('plus')){
+      type = 'plus'
+  }
+  updateQuantityInCart(type, product_id);
+}
+})
+const updateQuantityInCart = (type, product_id) => {
+    let productItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(productItemInCart >= 0){
+      let itemInfo = cart[productItemInCart];
+      switch (type){
+        case 'plus':
+          cart[productItemInCart].quantity = cart[productItemInCart].quantity + 1;
+          break;
+
+      default:
+        let updateQuantityInCart = cart[productItemInCart].quantity -1;
+        if (updateQuantityInCart > 0) {
+          cart[productItemInCart].quantity = updateQuantityInCart;
+        }else{
+          cart.splice(productItemInCart , 1);
+        }
+        break;
+      }
+    }
+    addItemCartToLocalStorage();
+    addItemCartToHTML();
+}
+const initApp = () => {
+// get data from json
+
+fetch('products.json')
+.then(response => response.json())
+.then(data => {
+  listProducts = data;
+  addDataProductItem();
+
+  // Get item from LocalStorage 
+   if(localStorage.getItem('cart')){
+    cart = JSON.parse(localStorage.getItem('cart'));
+    addItemCartToHTML();
+   }
+})
+}
+initApp();
